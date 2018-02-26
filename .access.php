26c93f2de501c062f6215d0ceebec1c8f72d7ddd
Averin Dmitry <averin@intaro.email>
2018-02-26 11:58:39 +0300

    init

---
+++ .access.php
@@ -0,0 +1,15 @@
+<?
+$PERM[".idea"]["2"]="D";
+$PERM[".idea"]["3"]="D";
+$PERM[".idea"]["4"]="D";
+$PERM[".idea"]["5"]="D";
+$PERM[".idea"]["*"]="D";
+$PERM["search"]["5"]="W";
+$PERM["products"]["5"]="W";
+$PERM["services"]["5"]="W";
+$PERM["news"]["5"]="W";
+$PERM["contacts"]["5"]="W";
+$PERM["company"]["5"]="W";
+$PERM["index.php"]["5"]="W";
+$PERM["/"]["*"]="R";
+?>
