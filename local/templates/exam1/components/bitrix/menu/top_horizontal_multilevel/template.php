6c23ffa320517b5396ddbbfe08d8eb0f7b7f6105
Averin Dmitry <averin@intaro.email>
2018-02-26 14:24:14 +0300

    ex1-6 top menu text

--- local/templates/exam1/components/bitrix/menu/top_horizontal_multilevel/template.php
+++ local/templates/exam1/components/bitrix/menu/top_horizontal_multilevel/template.php
@@ -7,22 +7,23 @@
                 <a href="" class="btn-menu btn-toggle"></a>
                 <div class="menu popup-block">
                     <ul class="">
-                        <li class="main-page"><a href="/"><?=GetMessage("MAIN")?></a></li>
-                        <?
-                        $previousLevel = 0;
+                        <li class="main-page"><a href="/"><?= GetMessage("MAIN") ?></a></li>
+                        <? $previousLevel = 0;
                         foreach ($arResult
 
                         as $arItem): ?>
-                        <? if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
+                        <? if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel): ?>
                             <?= str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"])); ?>
                         <? endif ?>
                         <? if ($arItem["IS_PARENT"]): ?>
                         <? if ($arItem["DEPTH_LEVEL"] == 1 or $arItem["PERMISSION"] > "D"): ?>
                         <li><a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a>
-                            <ul>
+                            <ul><? if (file_exists(\Bitrix\Main\Application::getDocumentRoot().$arItem["LINK"].'.top_menu_text.html')): ?>
+                                    <div class="menu-text"><?require(\Bitrix\Main\Application::getDocumentRoot().$arItem["LINK"].'.top_menu_text.html')?></div>
                                 <? endif ?>
-                                <? else:?>
-                                    <? if ($arItem["DEPTH_LEVEL"] == 1 or $arItem["PERMISSION"] > "D"):?>
+                                <? endif ?>
+                                <? else: ?>
+                                    <? if ($arItem["DEPTH_LEVEL"] == 1 or $arItem["PERMISSION"] > "D"): ?>
                                         <li><a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
                                     <? endif ?>
                                 <? endif ?>
