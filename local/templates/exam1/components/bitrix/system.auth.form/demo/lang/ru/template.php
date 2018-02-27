e581e2c3bfaab418ff7dfccdf7725a312085ae2c
Averin Dmitry <averin@intaro.email>
2018-02-27 16:38:46 +0300

    ex1-20 change

--- local/templates/exam1/components/bitrix/system.auth.form/demo/lang/ru/template.php
+++ local/templates/exam1/components/bitrix/system.auth.form/demo/lang/ru/template.php
@@ -6,6 +6,7 @@ $MESS["AUTH_LOGIN"] = "Логин";
 $MESS["AUTH_PASSWORD"] = "Пароль";
 $MESS["AUTH_REMEMBER_ME"] = "Запомнить меня на этом компьютере";
 $MESS["AUTH_REMEMBER_ME_SHORT"] = "Запомнить меня";
+$MESS["AUTH_FORGOT_PASSWORD"] = "Забыли пароль";
 $MESS["AUTH_FORGOT_PASSWORD_2"] = "Забыли пароль?";
 $MESS["AUTH_REGISTER"] = "Зарегистрироваться";
 $MESS["AUTH_LOGOUT_BUTTON"] = "Выйти";
